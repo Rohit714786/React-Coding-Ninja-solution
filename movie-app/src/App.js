604f24f5d9728e0d5362118bfe5a1f1ea5534b22
Rohit <rr37044@gmail.com>
2023-08-29 08:53:56 -0400

    Movie App completed with navbar with functional components

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,11 +1,83 @@
 import MovieList from "./MovieList";
-function App() {
-  return (
-    <>
-      <h1>Movie App</h1>
-      <MovieList />
-    </>
-  );
-}
+import Navbar from "./Navbar";
+import { movies } from "./moviesData";
+import React from "react";
 
-export default App;
+export default class App extends React.Component {
+  constructor() {
+    super();
+    //Creating the state object
+    this.state = {
+      movies: movies,
+      cartCount: 0,
+    };
+  }
+
+  handleAddStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars < 5) {
+      movies[movieId].stars += 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleDecStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars > 0) {
+      movies[movieId].stars -= 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].fav = !movies[movieId].fav;
+
+    this.setState({
+      movies,
+    });
+  };
+  handleAddtocart = (movie) => {
+    const { movies, cartCount } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].isInCart = !movies[movieId].isInCart;
+    const newCartCount = movies[movieId].isInCart
+      ? cartCount + 1
+      : cartCount - 1;
+    console.log(newCartCount);
+
+    this.setState({
+      movies,
+      cartCount: newCartCount,
+    });
+  };
+
+  render() {
+    const { movies, cartCount } = this.state;
+    return (
+      <>
+        <Navbar cartCount={cartCount} />
+        <MovieList
+          movies={movies}
+          onIncStars={this.handleAddStars}
+          onDecStars={this.handleDecStars}
+          onClickFav={this.handleToggleFav}
+          onClickAddtocart={this.handleAddtocart}
+        />
+      </>
+    );
+  }
+}
