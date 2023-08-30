1e92ffcc944ebff64d64a9f750bea2b835e9ef2c
Rohit <rr37044@gmail.com>
2023-08-30 07:40:17 -0400

    Movie App completed with navbar with functional component

--- movie-app/src/moviesData.js
+++ movie-app/src/moviesData.js
@@ -1,4 +1,5 @@
 export const movies = [
+  // Existing movies
   {
     title: "The Avengers",
     plot: "Plot of the movie is related to super natural powers",
@@ -10,25 +11,59 @@ export const movies = [
     fav: false,
     isInCart: false,
   },
-
   {
     title: "Ironman",
     plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
     price: 399,
     rating: 8.9,
     poster:
-      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+      "https://tse1.mm.bing.net/th?id=OIP.3GByvYL1xTT391g_RRpVNAHaLH&pid=Api&P=0&h=180",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  // Additional movies
+  {
+    title: "Inception",
+    plot: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    price: 299,
+    rating: 8.8,
+    poster:
+      "https://tse1.mm.bing.net/th?id=OIP.6WES5nhdyXVDPrxHG0vCvgHaKz&pid=Api&P=0&h=180",
     stars: 0,
     fav: false,
     isInCart: false,
   },
   {
-    title: "The Avengers",
-    plot: "Plot of the movie is related to super natural powers",
-    price: 199,
-    rating: 8.0,
+    title: "The Matrix",
+    plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+    price: 349,
+    rating: 8.7,
     poster:
-      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+      "https://tse4.mm.bing.net/th?id=OIP.Nuy2-f-oU2QUSZ8alJzX3gHaLH&pid=Api&P=0&h=180",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  // Add more movies below
+  {
+    title: "Jurassic Park",
+    plot: "During a preview tour, a theme park suffers a major power breakdown that allows its cloned dinosaur exhibits to run amok.",
+    price: 249,
+    rating: 8.1,
+    poster:
+      "https://tse1.mm.bing.net/th?id=OIP.GbS_DXOZgAwkYyH4j_nkwwHaLQ&pid=Api&P=0&h=180",
+    stars: 0,
+    fav: false,
+    isInCart: false,
+  },
+  {
+    title: "Avatar",
+    plot: "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    price: 299,
+    rating: 7.8,
+    poster:
+      "https://tse2.mm.bing.net/th?id=OIP.8f4LOs5CeR0LBTqiphcpCQHaK9&pid=Api&P=0&h=180",
     stars: 0,
     fav: false,
     isInCart: false,
